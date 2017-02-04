1db6ea3bd4b68ab56e133cdc899dc9cce23f1c0f
Steve Cui <stevecui@Steves-MacBook-Pro.local>
2017-02-04 12:30:45 -0800

    s

---
+++ Social/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Social
+//
+//  Created by Steve Cui on 2/4/17.
+//  Copyright © 2017 Kevin Cui. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
